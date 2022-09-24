0a008f955375a5ae98741120d69d8475ff904dd5
philipdaquin <philipdaquin20@gmail.com>
2022-09-24 10:24:09 +0800

    added registration classes

---
+++ src/main/java/com/example/crud_java_project/registration/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.example.crud_java_project.registration;
+
+public class RegistrationService {
+
+    public static String register(RegistrationRequest request) {
+        return "IT WORKS MUFAFFFKA";
+    }
+
+}
