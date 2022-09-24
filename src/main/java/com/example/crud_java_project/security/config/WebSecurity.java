ab20a974096d1f39c49f0a6e1c082a4f7c533fce
philipdaquin <philipdaquin20@gmail.com>
2022-09-24 11:57:16 +0800

    web security

--- src/main/java/com/example/crud_java_project/security/config/WebSecurity.java
+++ src/main/java/com/example/crud_java_project/security/config/WebSecurity.java
@@ -1,5 +1,73 @@
 package com.example.crud_java_project.security.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import com.example.crud_java_project.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 
-public class WebSecurity {
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurity  {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    
+    //  We want to secure the endpoints depending on the roles and leave an anonymous 
+    // entry point only for login 
+    //  Restric any DELETE request to an admin role 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
+        http.csrf()
+            .disable()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.DELETE)
+            .hasRole("ADMIN")
+            .antMatchers("/admin/**")
+            .hasAnyRole("ADMIN")
+            .antMatchers("/user/**")
+            .hasAnyRole("USER", "ADMIN")
+            .antMatchers("/api/v*/registration/**")
+            .anonymous()
+            .anyRequest()
+            // .authenticated()
+            .permitAll()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    
+
+
+
     
 }
