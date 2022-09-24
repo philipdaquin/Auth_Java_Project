e3f0b29fbd7ff6bf5bcfa65221f3fe01c1f6b79f
philipdaquin <philipdaquin20@gmail.com>
2022-09-24 14:05:37 +0800

    created register method with throw exception handling using the AppUser constructor, edited appuser

--- src/main/java/com/example/crud_java_project/appuser/AppUser.java
+++ src/main/java/com/example/crud_java_project/appuser/AppUser.java
@@ -36,54 +36,53 @@ public class AppUser implements UserDetails {
         generator = "student_sequence"
     )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING) 
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
     public AppUser(
-        Long id,
-        String name, 
-        String username, 
+        String firstName, 
+        String lastName, 
         String email, 
         String password, 
-        AppUserRole appUserRole, 
-        Boolean locked, 
-        Boolean enabled
+        AppUserRole appUserRole
     ) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
     
-
-
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
         return Collections.singletonList(authority);
     }
 
+    public String getFirstName() {
+        // TODO Auto-generated method stub
+        return firstName;
+    }
+    public String getLastname() {
+        return lastName;
+    }
+    
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return email;
+    }
+
     @Override
     public String getPassword() {
         // TODO Auto-generated method stub
         return password;
-    }
-
-    @Override
-    public String getUsername() {
-        // TODO Auto-generated method stub
-        return username;
     }
 
     @Override
@@ -109,5 +108,7 @@ public class AppUser implements UserDetails {
         // TODO Auto-generated method stub
         return enabled;
     }
+
+   
     
 }
--- src/main/java/com/example/crud_java_project/registration/RegistrationService.java
+++ src/main/java/com/example/crud_java_project/registration/RegistrationService.java
@@ -2,11 +2,32 @@ package com.example.crud_java_project.registration;
 
 import org.springframework.stereotype.Service;
 
+import com.example.crud_java_project.appuser.AppUser;
+import com.example.crud_java_project.appuser.AppUserRole;
+import com.example.crud_java_project.appuser.AppUserService;
+
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
-    public static String register(RegistrationRequest request) {
-        return "IT WORKS MUFAFFFKA";
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) throws Exception {
+        boolean isValid = emailValidator.test(request.getEmail());
+        if (!isValid) throw new IllegalStateException("Email Not Valid");
+
+        return appUserService.signUpUser(
+            new AppUser(
+                request.getFirstName(),
+                request.getEmail(),
+                request.getLastName(), 
+                request.getPassword(),
+                AppUserRole.USER
+            )
+        );
     }
 
 }
