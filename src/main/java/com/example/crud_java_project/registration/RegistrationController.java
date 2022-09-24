7dda208ba952d1df05e6ca79f57792af94fff69c
philipdaquin <philipdaquin20@gmail.com>
2022-09-24 14:04:49 +0800

    fixed registration service, added postmapping

--- src/main/java/com/example/crud_java_project/registration/RegistrationController.java
+++ src/main/java/com/example/crud_java_project/registration/RegistrationController.java
@@ -1,5 +1,6 @@
 package com.example.crud_java_project.registration;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,9 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) { 
-        return RegistrationService.register(request);
+        System.out.printf("Received a new request %s", request);
+        return registrationService.register(request);
     }
 }
